31ad2461fc4061910ddfe7a7c93b45d726a45d04
David Fernández <149385933+davidfb1305@users.noreply.github.com>
2026-09-23 14:41:57 +0200

    proyecto y el ejemplo de giroscopio de la doc

---
+++ gyroTestCom.gd
@@ -0,0 +1,41 @@
+extends MeshInstance3D
+
+
+const GYRO_SENSITIVITY = 10.0
+
+func _ready():
+	# In this example we only use the first connected joypad (id 0).
+	if 0 not in Input.get_connected_joypads():
+		return
+
+	if not Input.has_joy_motion_sensors(0):
+		return
+
+	# We must enable the motion sensors before using them.
+	Input.set_joy_motion_sensors_enabled(0, true)
+
+	# (Tell the users here that they need to put their joypads on a flat surface and wait for confirmation.)
+
+	# Start the calibration process.
+	calibrate_motion()
+
+func _process(delta):
+	# Only move the object if the joypad motion sensors are calibrated.
+	if Input.is_joy_motion_sensors_calibrated(0):
+		move_object(delta)
+
+func calibrate_motion():
+	Input.start_joy_motion_sensors_calibration(0)
+
+	# Wait for some time.
+	await get_tree().create_timer(1.0).timeout
+
+	Input.stop_joy_motion_sensors_calibration(0)
+	# The joypad is now calibrated.
+
+func move_object(delta):
+	var node: Node3D = self
+
+	var gyro := Input.get_joy_gyroscope(0)
+	node.rotation.x -= -gyro.y * GYRO_SENSITIVITY * delta  # Use rotation around the Y axis (yaw) here.
+	node.rotation.y += -gyro.x * GYRO_SENSITIVITY * delta  # Use rotation around the X axis (pitch) here.
